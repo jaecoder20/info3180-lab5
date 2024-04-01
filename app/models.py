9a127d7367024a2222b69be2ac24743764a79283
Rojae Wedderburn <rojaewedderburn@gmail.com>
2024-04-01 11:39:58 -0500

    Implement movie database table
    Added PostgreSQL database named "lab5" and a table
     called "movies" to store  movies. Utilized the
    models.py file to create a Movie class with columns:
    id (integer), title (string), description (text),
    poster (string), and created_at (datetime).
    Implemented Flask Migrate and Flask SQLAlchemy for
     database management and created a migration file.
     Updated the .env file and created uploads folder
     to store poster images.

--- app/models.py
+++ app/models.py
@@ -1,1 +1,21 @@
-# Add any model classes for Flask-SQLAlchemy here
+import datetime
+from . import db
+
+class Movies(db.Model):
+    __tablename__ = 'movies'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    poster_url = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.datetime.now())
+
+    def __init__(self, id, title, description, poster_url):
+        self.id = id
+        self.title = title
+        self.description = description
+        self.poster_url = poster_url
+
+
+    def __repr__(self):
+        return f"Movie({self.title})"
